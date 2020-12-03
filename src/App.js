8db6542f8daaa3037c3b110fb66cea725da67de6
rrosmann <raphael.rosmann@gmx.at>
2020-12-03 13:19:25 +0100

    added basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import './pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div className='App'>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path='/' component={Homepage}></Route>
+      </Switch>
     </div>
   );
 }
